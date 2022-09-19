4338a0dea126f98d62420399ac994d4b0c89ad92
Emir <emir@degirmenci.dk>
2022-09-19 14:48:51 +0200

    Facts

--- src/ASCIItabel.java
+++ src/ASCIItabel.java
@@ -14,15 +14,15 @@ public class ASCIItabel { public static void main(String[]args){
     if(c >= 65 && c <= 90) {
         System.out.println("Tegnet er et stort bogstav");   }
                  else if (c >= 97 && c <=122 ){
-            System.out.println("Tegnet er et lille bogstav");  }
+                     System.out.println("Tegnet er et lille bogstav");  }
                  else if(c >= 48 && c <= 57){
-            System.out.println("Tegnet er et tal / for 0-9"); }
+                     System.out.println("Tegnet er et tal / for 0-9"); }
                  else if(c == 33 || c ==35 || c == 43 || c == 47){
-            System.out.println("Tegnet af et af de 4 specialtegn");  }
-                    else {
-                System.out.println("Tegnet er ikke kendt af dette programet");
+                     System.out.println("Tegnet af et af de 4 specialtegn");  }
+                 else {
+                     System.out.println("Tegnet er ikke kendt af dette programet");
             }
+                 System.out.println("Emir er sindsyg til det der git");
     }
 }
 }
-                  //asdasdasd
