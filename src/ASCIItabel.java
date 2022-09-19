4bcbe334419950cfd1f1b6f74d7fa60be656646c
Emir <emir@degirmenci.dk>
2022-09-19 15:05:07 +0200

    ændret i linje 23

--- src/ASCIItabel.java
+++ src/ASCIItabel.java
@@ -25,7 +25,9 @@ public class ASCIItabel { public static void main(String[]args){
                  System.out.println("Emir er sindsyg til det der git");
                  System.out.println("Ahmad er næsten ligeså god som Emir");
                  System.out.println("Hossain er næsten lige så god som ahmad");
-        System.out.println("opagve");
+                 System.out.println("opagve");
+
+                 System.out.println("Hej");
     }
 }
 }
